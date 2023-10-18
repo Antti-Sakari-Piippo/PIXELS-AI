128944b05b00d7ab9563508fdcd2775dfdee2656
AnttiPiippo <antti.sakari.piippo@gmail.com>
2023-10-18 16:22:13 +0300

    improve accessability

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const hamburger = document.querySelector('.hamburger')
 const navToggle = document.getElementById('navToggle')
 const navMenu = document.getElementById('navMenu')
+const questions = document.querySelectorAll('.faq-page')
 
-////////// Navbar
-navToggle.addEventListener('click', () => {
+// Navbar
+function toggleNavigation() {
   const expanded = navToggle.getAttribute('aria-expanded') === 'true' || false
   navToggle.setAttribute('aria-expanded', !expanded)
+}
+
+// Toggle the navigation menu when the hamburger button is clicked
+navToggle.addEventListener('click', () => {
+  toggleNavigation()
 })
 
+// Toggle the navigation menu when the Enter key is pressed on the hamburger button
+hamburger.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    toggleNavigation()
+  }
+})
+
+// Toggle the navigation menu when the Enter key is pressed on the hamburger button
+hamburger.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    toggleNavigation()
+  }
+})
+// Click outside to close the navigation menu
 document.addEventListener('click', (event) => {
   if (
     event.target !== navMenu &&
@@ -17,7 +37,6 @@ document.addEventListener('click', (event) => {
     navToggle.setAttribute('aria-expanded', 'false')
   }
 })
-
 // Update the navigation state when the window is resized
 window.addEventListener('resize', () => {
   if (window.innerWidth > 1100) {
@@ -27,13 +46,28 @@ window.addEventListener('resize', () => {
   }
 })
 
-///////// Accordion
-const questions = document.querySelectorAll('.faq-page')
-
+// Accordion
 questions.forEach((item) => {
   item.addEventListener('click', () => {
     const faqBody = item.parentElement.querySelector('.faq-body')
     faqBody.classList.toggle('active')
     item.classList.toggle('rotate')
+  })
+})
+
+// Aria for Accordion
+questions.forEach((button) => {
+  button.addEventListener('click', () => {
+    const faqBody = button.parentElement.querySelector('.faq-body')
+    const expanded = button.getAttribute('aria-expanded') === 'true'
+    button.setAttribute('aria-expanded', !expanded)
+    faqBody.setAttribute('aria-hidden', expanded)
+  })
+
+  button.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+      // Enter key
+      button.click()
+    }
   })
 })
